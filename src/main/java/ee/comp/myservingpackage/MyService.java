0a522db5d48ab69d40ce4f8659244f9993ecb4d8
Mihkel Ehrlich <mihkel.ehrlich@icefire.ee>
2017-09-13 15:56:54 +0300

    MyService rename "b" to "books"

--- src/main/java/ee/comp/myservingpackage/MyService.java
+++ src/main/java/ee/comp/myservingpackage/MyService.java
@@ -5,46 +5,46 @@ import java.util.List;
 
 public class MyService {
 
-    private List b;
+    private List books;
 
 
     public boolean isAllowed(){
-        if(b.size()==5){ //Maximum 5 books can be added
+        if(books.size()==5){ //Maximum 5 books can be added
             return false;
         }
         return true;
     }
 
     public void add(Object book){
-        if(b == null){
-            b = new ArrayList();
+        if(books == null){
+            books = new ArrayList();
         }
 
         if(isAllowed()){
-            b.add(book);
+            books.add(book);
         }
     }
 
     public List getNewItemsArrayWhenNull(){
-        if(b == null){
-            b = new ArrayList();
+        if(books == null){
+            books = new ArrayList();
         }
-        return b;
+        return books;
     }
 
     public int getOSS(){
         int pi = 0;
-        for(Object p: b)if(p instanceof NewBookInfo)pi+= 1;
+        for(Object p: books)if(p instanceof NewBookInfo)pi+= 1;
         return pi;
     }
 
     public boolean anyBooks(){
-        int a = b.size();
+        int a = books.size();
         return a>0;
     }
 
     public List displaymystuff(){
-        for(Object o: b){
+        for(Object o: books){
             if(o instanceof OldBookDataHolder){
                 System.out.println(((OldBookDataHolder) o).getOt());
             }else if(o instanceof NewBookInfo){
